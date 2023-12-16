438f11424e110b26e73e56dcc675ada72d42f1ab
Jessica Nelson <jessica.nelson60@gmail.com>
2023-12-16 10:40:38 -0700

    added responsiveness to hero section

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,18 +1,18 @@
-<main class="h-screen relative">
+<main class="relative flex items-center justify-center h-screen">
   <!-- Background image of the condo in Mexico -->
-  <img src="/images/pvmx-hero.jpg" class="w-full h-full object-cover" alt="Condo in Mexico"> 
+  <img src="/images/pvmx-hero.jpg" class="w-full h-full object-cover" alt="Condo in Mexico">
 
   <!-- Text section over the image with a semi-transparent background -->
-  <section class="absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white bg-black bg-opacity-30 p-8 rounded-lg">
+  <section class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white bg-black bg-opacity-30 p-8 rounded-lg max-w-md mx-auto">
     <!-- Dynamic greeting text with a gradient background -->
-    <h1 class="text-4xl font-bold mb-4">
+    <h1 class="text-3xl md:text-4xl lg:text-5xl mb-4">
       Discover Your Perfect Getaway in Mexico
     </h1>
-    <p class="text-lg mb-6">
+    <p class="text-base md:text-lg lg:text-xl mb-6">
       Immerse yourself in luxury at our beautiful condo. <br>Book now for an unforgettable experience!
     </p>
     <!-- Call-to-action button -->
-    <a href="#contact" class="px-8 py-3 bg-blue-500 rounded-lg text-white hover:bg-blue-600 transition duration-300">
+    <a href="#contact" class="px-6 py-3 bg-blue-500 rounded-lg text-white hover:bg-blue-600 transition duration-300 inline-block">
       Book Now
     </a>
   </section>

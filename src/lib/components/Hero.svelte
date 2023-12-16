7e5fecc3596d9ab293380c6f385241e60da876f6
Jessica Nelson <jessica.nelson60@gmail.com>
2023-12-16 06:57:48 -0700

    created rough draft of hero section

---
+++ src/lib/components/Hero.svelte
@@ -0,0 +1,19 @@
+<main class="h-screen relative">
+  <!-- Background image of the condo in Mexico -->
+  <img src="/images/pvmx-hero.jpg" class="w-full h-full object-cover" alt="Condo in Mexico"> 
+
+  <!-- Text section over the image -->
+  <section class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white">
+    <!-- Dynamic greeting text with a gradient background -->
+    <h1 class="text-4xl font-bold mb-4">
+      Discover Your Perfect Getaway in Mexico
+    </h1>
+    <p class="text-lg mb-6">
+      Immerse yourself in luxury at our beautiful condo. Book now for an unforgettable experience!
+    </p>
+    <!-- Call-to-action button -->
+    <a href="#contact" class="px-8 py-3 bg-blue-500 rounded-lg text-white hover:bg-blue-600 transition duration-300">
+      Book Now
+    </a>
+  </section>
+</main>

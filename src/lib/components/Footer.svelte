2aadd9422ec0588677995c8749bab4d19659d6ed
Jessica Nelson <jessica.nelson60@gmail.com>
2023-12-16 10:35:49 -0700

    added responsiveness to footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -8,12 +8,14 @@
 </script>
 
 <footer class="bg-[#cee3c1] py-4">
-    <nav class="flex justify-center">
+  <div class="container mx-auto flex flex-col items-center">
+    <nav class="flex flex-wrap justify-center space-x-2 md:space-x-5 lg:space-x-8">
       {#each navItems as { name, href }}
-        <a href={href} class="text-lg md:text-xl lg:text-2xl text-green-900 hover:text-green-700 hover:underline transition-colors duration-300 px-5 py-2 rounded-md focus:outline-none focus:shadow-outline">
+        <a href={href} class="text-base md:text-lg lg:text-xl text-green-900 hover:text-green-700 hover:underline transition-colors duration-300 px-2 md:px-4 py-2 rounded-md focus:outline-none focus:shadow-outline">
           {name}
         </a>
       {/each}
     </nav>
-    <p class="flex justify-center text-sm mt-4 text-green-900">&copy; 2023 PVMX</p>
+    <p class="text-xs md:text-sm mt-4 text-green-900">&copy; 2023 PVMX</p>
+  </div>
 </footer>

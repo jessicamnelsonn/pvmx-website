e71425c009e85e1c32718895c86c7da0cbfd3da1
Jessica Nelson <jessica.nelson60@gmail.com>
2023-12-16 07:14:33 -0700

    added extra stlying to enhance readability

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -2,14 +2,14 @@
   <!-- Background image of the condo in Mexico -->
   <img src="/images/pvmx-hero.jpg" class="w-full h-full object-cover" alt="Condo in Mexico"> 
 
-  <!-- Text section over the image -->
-  <section class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white">
+  <!-- Text section over the image with a semi-transparent background -->
+  <section class="absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white bg-black bg-opacity-30 p-8 rounded-lg">
     <!-- Dynamic greeting text with a gradient background -->
     <h1 class="text-4xl font-bold mb-4">
       Discover Your Perfect Getaway in Mexico
     </h1>
     <p class="text-lg mb-6">
-      Immerse yourself in luxury at our beautiful condo. Book now for an unforgettable experience!
+      Immerse yourself in luxury at our beautiful condo. <br>Book now for an unforgettable experience!
     </p>
     <!-- Call-to-action button -->
     <a href="#contact" class="px-8 py-3 bg-blue-500 rounded-lg text-white hover:bg-blue-600 transition duration-300">

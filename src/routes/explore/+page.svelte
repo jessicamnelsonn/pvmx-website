98dd8d7bba8c00ef99752c918fe14db2e622009d
Jessica Nelson <jessica.nelson60@gmail.com>
2023-12-23 07:34:24 -0700

    added explore page info and styling

---
+++ src/routes/explore/+page.svelte
@@ -0,0 +1,29 @@
+<section class="py-8 px-4 lg:px-8 bg-gray-100">
+    <h2 class="text-3xl lg:text-4xl xl:text-5xl font-bold text-green-900 mb-8 text-center">Explore Our Amenities</h2>
+
+    <!-- Condo Amenities -->
+    <div class="mb-10">
+      <h3 class="text-2xl lg:text-3xl xl:text-4xl font-semibold mb-4 text-green-900">Condo Features</h3>
+      <ul class="list-disc">
+
+        <!-- First Floor -->
+        <span class="font-bold text-green-700">First Floor:</span>
+        <ul class="list-disc pl-6">
+            <li class="mb-2">Bedroom with Full Bathroom and Closet</li>
+            <li class="mb-2">Washer and Dryer</li>
+            <li class="mb-2">1⁄2 Bathroom</li>
+            <li class="mb-2">Kitchen with Bar</li>
+            <li class="mb-2">Living Room</li>
+            <li class="mb-2">Dining Room</li>
+            <li class="mb-2">Terrace</li>
+          </ul>
+
+        <!-- Second Floor -->
+          <span class="font-bold text-green-700">Second Floor:</span>
+          <ul class="list-disc pl-6">
+            <li class="mb-2">Master Bedroom with Full Bathroom and Closet</li>
+            <li class="mb-2">Secondary Bedroom with Full Bathroom and Closet</li>
+          </ul>
+      </ul>
+    </div>
+</section>
